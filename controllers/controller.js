3abfe68dc7622c77bac45bfd85809a3a5ecc4259
Yadira Cervantes <cervantes.yadira@student.greenriver.edu>
2025-03-04 16:49:24 -0800

    Fixed the floating footer, extracted script from the project-outline file, and removed unnecessary docs

--- controllers/controller.js
+++ controllers/controller.js
@@ -6,94 +6,21 @@
 
 const path = require("path");
 
-/**
- * Renders the home page.
- * 
- * @function getHome
- * @param {Object} req - The request object.
- * @param {Object} res - The response object.
- */
-const getHome = (req, res) => {
-    res.render('index', {title: 'GRC Aviation'});
-}
+const getHome = (req, res) => res.render('index', {title: 'GRC Aviation'});
 
-/**
- * Renders the Programs page.
- * 
- * @function getPrograms
- * @param {Object} req - The request object.
- * @param {Object} res - The response object.
- */
-const getPrograms = (req, res) => {
-    res.render('programs', {title: 'Programs'});
-}
+const getPrograms = (req, res) => res.render('programs', {title: 'Programs'});
 
-/**
- * Renders the Resources page.
- * 
- * @function getResources
- * @param {Object} req - The request object.
- * @param {Object} res - The response object.
- */
-const getResources = (req, res) => {
-    res.render('resources', {title: 'Resources'});
-}
+const getResources = (req, res) => res.render('resources', {title: 'Resources'});
 
-/**
- * Renders the Contact page.
- * 
- * @function getContact
- * @param {Object} req - The request object.
- * @param {Object} res - The response object.
- */
-const getContact = (req, res) => {
-    res.render('contact', {title: 'Contact'});
-}
+const getContact = (req, res) => res.render('contact', {title: 'Contact'});
 
+const getProgramOutline = (req, res) => res.render('program-outline', {title: 'Program Outline'});
 
-/**
- * Renders the Program Outline page.
- * 
- * @function getProgramOutline
- * @param {Object} req - The request object.
- * @param {Object} res - The response object.
- */
-const getProgramOutline = (req, res) => {
-    res.render('program-outline', {title: 'Program Outline'});
-}
+const getFAQ = (req, res) => res.render('faq', {title: 'FAQ'});
 
-/**
- * Renders the FAQ page.
- * 
- * @function getFAQ
- * @param {Object} req - The request object.
- * @param {Object} res - The response object.
- */
-const getFAQ = (req, res) => {
-    res.render('faq', {title: 'FAQ'});
-}
+const getLogIn = (req, res) => res.render('log-in', {title: 'Admin Log In'});
 
-/**
- * Renders the Log-In page.
- * 
- * @function getLogIn
- * @param {Object} req - The request object.
- * @param {Object} res - The response object.
- */
-const getLogIn = (req, res) => {
-    res.render('log-in', {title: 'Admin Log In'});
-}
-
-/**
- * Renders the Documents page.
- * 
- * @function getDocuments
- * @param {Object} req - The request object.
- * @param {Object} res - The response object.
- */
-const getDocuments = (req, res) => {
-    res.render('documents', {title: 'Documents'});
-}
+const getDocuments = (req, res) => res.render('documents', {title: 'Documents'});
 
 const getDispachTrackAviationTechPlan = (req, res) => { 
     const fileName = req.params.fileName; 
