14688a87286e99dd75a4f249db60625a149351ea
Tien Nguyen <nguyen.tien@student.greenriver.edu>
2025-03-13 12:13:46 -0700

    added a feature where you can edit the newsletter link

--- public/Scripts/script.js
+++ public/Scripts/script.js
@@ -1,3 +1,5 @@
+
+document.addEventListener("DOMContentLoaded", editNewsLetter);
 // Set the src of the iframe based on the clicked link
 function setPdfSrc(link) {
     const pdfUrl = link.href; // Get the href of the clicked link
@@ -7,4 +9,26 @@ function setPdfSrc(link) {
 // Clear the iframe src when modal is closed
 function clearPdfSrc() {
     document.getElementById("pdfViewer").src = ''; // Clear the iframe src
+}
+
+// Function for edit newsletter link 
+function editNewsLetter() { 
+    const link = document.querySelector(".flying-gator");
+    const editNewsLetterButton = document.querySelector(".editNewsLetterButton")
+
+
+
+    // Load the saved URL from localStorage (if exists)
+    const savedUrl = localStorage.getItem("flyingGatorURL");
+    if (savedUrl) {
+      link.href = savedUrl;
+    }
+
+    editNewsLetterButton.addEventListener("click", function () {
+      const newUrl = prompt("Enter a new URL:", link.href);
+      if (newUrl) {
+        link.href = newUrl;
+        localStorage.setItem("flyingGatorURL", newUrl); // Save it to localStorage
+      }
+    });
 }
